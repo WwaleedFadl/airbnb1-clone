00e03c779d84e2835efcea69951515dc496bfa3a
waleedfadl <wwaleedfadl@gmail.com>
2025-04-23 03:09:15 +0200

    adding part of the modal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,7 +1,13 @@
 "use client";
+import { useState, useCallback } from 'react'
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
+import MenuItem from './MenuItem';
 const UserMenu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleOpen = useCallback(() => {
+    setIsOpen((value) => !value)
+  }, [])
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
@@ -12,7 +18,7 @@ const UserMenu = () => {
           Airbnb your home
         </div>
         <div
-          onClick={() => { }}
+          onClick={toggleOpen}
           className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full  cursor-pointer hover:shadow-md transition"
         >
           <AiOutlineMenu />
@@ -21,6 +27,17 @@ const UserMenu = () => {
           </div>
         </div>
       </div>
+      {isOpen && (
+        <div className='absolute rounded-xl shadow-md w-[40vw] md:w-[3/4]
+          bg-white overflow-hidden right-0 top-12 text-sm'>
+          <div className='flex flex-col cursor-pointer'>
+            <>
+              <MenuItem onClick={() => { }} label='Login' />
+              <MenuItem onClick={() => { }} label='Signup' />
+            </>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
