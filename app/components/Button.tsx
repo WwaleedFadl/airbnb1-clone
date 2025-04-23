fa56b44b4a6bdaab826cc248b1f31904159dd1c6
waleedfadl <wwaleedfadl@gmail.com>
2025-04-23 03:27:06 +0200

    button part

---
+++ app/components/Button.tsx
@@ -0,0 +1,19 @@
+import { IconType } from "react-icons";
+
+interface ButtonProps {
+  label: string;
+  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+  outline?: boolean;
+  small?: boolean;
+  icon?: IconType;
+}
+const Button: React.FC<ButtonProps> = ({
+  label, onClick, disabled, outline, small, icon
+}) => {
+  return (
+    <button>click</button>
+  )
+}
+
+export default Button
