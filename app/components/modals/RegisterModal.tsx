19e4c62b8b5faa5e11954f06eead1475b8f74a5f
waleedfadl <wwaleedfadl@gmail.com>
2025-04-24 17:53:43 +0200

    register ui

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -6,6 +6,8 @@ import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import useRegisterModal from '@/app/hooks/userRegisterModal'
 import Modal from './Modal'
+import Heading from '../Heading'
+import Input from '../inputs/Input'
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -30,6 +32,23 @@ const RegisterModal = () => {
       })
   }
 
+  const bodyContent = (
+    <div className='flex flex-col gap-4'>
+      <Heading
+        title="Welcome to Airbnb"
+        subtitle='Create an account!'
+      />
+      <Input
+        id='email'
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+    </div>
+  )
+
   return (
     <Modal
       disabled={isLoading}
@@ -38,6 +57,7 @@ const RegisterModal = () => {
       actionLabel='Continue'
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
+      body={bodyContent}
     />
   )
 }
