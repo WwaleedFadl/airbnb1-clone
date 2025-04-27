1d253ae1ec8a0ab9f8e04ed6bfb3b82b34315cfc
waleedfadl <wwaleedfadl@gmail.com>
2025-04-27 17:51:15 +0300

    hope it will work

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -12,9 +12,7 @@ export async function POST(request: Request) {
   const hashedPassword = await bcrypt.hash(password, 12);
   const user = await prisma.user.create({
     data: {
-      email,
-      name,
-      hashedPassword
+      email, name, hashedPassword
     }
   })
   return NextResponse.json(user)
