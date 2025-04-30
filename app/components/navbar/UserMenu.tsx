48f2c414c9cb46fb6a49ed14041d9bf5e9a1dcfc
waleedfadl <wwaleedfadl@gmail.com>
2025-04-30 20:41:10 +0300

    adding rentModal

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,6 +7,7 @@ import useRegisterModal from '@/app/hooks/userRegisterModal';
 import useLoginModal from '../../hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
+import useRentModal from '@/app/hooks/useRentModal';
 
 interface UserMenuProps {
   currentUser?: SafeUser | null
@@ -18,14 +19,22 @@ const UserMenu: React.FC<UserMenuProps> = ({
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal()
   const [isOpen, setIsOpen] = useState(false)
+  const rentModal = useRentModal()
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value)
   }, [])
+  const onRent = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen()
+    }
+    // open rent modal 
+    rentModal.onOpen()
+  }, [currentUser, loginModal, rentModal])
   return (
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div
-          onClick={() => { }}
+          onClick={onRent}
           className="hidden md:block text-sm font-semibold py-3 px-4 rounded-full hover:bg-neutral-100 transition cursor-pointer"
         >
           Airbnb your home
