3b05de86535d57f3ea1f82bc5cd48b280964157b
waleedfadl <wwaleedfadl@gmail.com>
2025-05-03 06:46:21 +0300

    adding heart

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
 
 interface HeartButtonProps {
@@ -10,8 +11,17 @@ const HeartButton: React.FC<HeartButtonProps> = ({
   listingId,
   currentUser
 }) => {
+  const hasFavorited = false;
+  const toggleFavorite = () => { }
   return (
-    <div>
+    <div
+      onClick={toggleFavorite}
+      className="relative hover:opacity-80 transition cursor-pointer"
+    >
+      <AiOutlineHeart
+        size={28}
+        className='fill-white absolute -top-[2px] -right-[2px]'
+      />
     </div>
   )
 }
