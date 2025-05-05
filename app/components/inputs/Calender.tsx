43a6fc5a217b347ee0dd5b0686f1113047693636
waleedfadl <wwaleedfadl@gmail.com>
2025-05-05 14:36:53 +0300

    adding calender

--- app/components/inputs/Calender.tsx
+++ app/components/inputs/Calender.tsx
@@ -1,7 +1,32 @@
 'use client'
-const Calender = () => {
+
+import { DateRange, Range, RangeKeyDict } from "react-date-range"
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
+
+interface CalenderProps {
+  value: Range;
+  onChange: (value: RangeKeyDict) => void;
+  disabledDates?: Date[]
+}
+
+
+const Calender: React.FC<CalenderProps> = ({
+  value,
+  onChange,
+  disabledDates
+}) => {
   return (
-    <div></div>
+    <DateRange
+      rangeColors={['#262525']}
+      ranges={[value]}
+      date={new Date()}
+      onChange={onChange}
+      direction="vertical"
+      showDateDisplay={false}
+      minDate={new Date()}
+      disabledDates={disabledDates}
+    />
   )
 }
 
