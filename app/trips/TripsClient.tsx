ee7dabb307d2b19f954450ccc2afcacf3d93cadc
waleedfadl <wwaleedfadl@gmail.com>
2025-05-06 10:15:13 +0300

    adding trip page

--- app/trips/TripsClient.tsx
+++ app/trips/TripsClient.tsx
@@ -1,7 +1,65 @@
 'use client'
-const TripsClient = () => {
+
+import toast from "react-hot-toast";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { SafeReservation, SafeUser } from "../types"
+import { useRouter } from 'next/navigation'
+import { useState, useCallback } from 'react'
+
+
+interface TripsClientProps {
+  reservations: SafeReservation;
+  currentUser?: SafeUser | null;
+}
+const TripsClient: React.FC<TripsClientProps> = ({
+  reservations,
+  currentUser
+}) => {
+  const router = useRouter()
+  const [deletingId, setDeletingId] = useState('')
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id)
+      axios.delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast..success("Reservations cancelled")
+          router.refresh()
+        })
+        .catch((error) => {
+          toast.error(error?.response?.data?.error)
+        })
+        .finally(() => {
+          setDeletingId('')
+        })
+    },
+    [router],
+  )
+
+
   return (
-    <div>My Trips</div>
+    <Container>
+      <Heading title="Trips" subtitle="Where you've been and where you're going to" />
+      <div
+        className='mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'
+      >
+        {
+          reservations.map((reservation) => (
+            <ListingCard
+              key={reservation.id}
+              data={reservation.listing}
+              reservation={reservation}
+              actionId={reservation.id}
+              onAction={onCancel}
+              disabled={deletingId === reservation.id}
+              actionLabel="Cancel Reservation"
+              currentUser={currentUser}
+            />
+          ))
+        }
+      </div>
+    </Container>
   )
 }
 
