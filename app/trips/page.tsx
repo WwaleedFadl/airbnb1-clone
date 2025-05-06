dafdea5740e0651c9c77e8e1649cc2aeefcc7eea
waleedfadl <wwaleedfadl@gmail.com>
2025-05-06 10:20:46 +0300

    small fix

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,4 +1,4 @@
-'user client'
+'use client'
 import EmptyState from "../components/EmptyState"
 import ClientOnly from "../components/ClientOnly"
 import getCurrentUser from "../actions/getCurrentUser"
