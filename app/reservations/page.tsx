009d2a5717b2016ba16a965ff3d6ff4c27574398
waleedfadl <wwaleedfadl@gmail.com>
2025-05-07 12:02:30 +0300

    hopee it works

---
+++ app/reservations/page.tsx
@@ -0,0 +1,9 @@
+import EmptyState from '../components/EmptyState'
+import ClientOnly from '../components/ClientOnly'
+const page = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default page
