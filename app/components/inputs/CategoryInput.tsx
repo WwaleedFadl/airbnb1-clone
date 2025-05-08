cf864d4de31167b4159ca866f8a04d24d2086714
waleedfadl <wwaleedfadl@gmail.com>
2025-05-08 07:52:52 +0300

    adding search

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -23,7 +23,7 @@ const CategoryInput: React.FC<CategoryInputProps> = (
       className={`rounded-xl border-2 p-4 flex flex-col gap-3 hover:border-black transition cursor-pointer ${selected ? "border-black" : "border-neutral-200"}`}
     >
       <Icon size={30} />
-      <div calssName='font-semibold'>
+      <div className='font-semibold'>
         {label}
       </div>
     </div>
